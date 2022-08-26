ebe23d8aff2bbe86c87bc0e1df0530026b793ae6
Paulo Costa <paulofrontend@gmail.com>
2022-08-26 13:13:22 -0300

    fix eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,6 +18,9 @@ module.exports = {
     "parserOptions": {
         "ecmaVersion": "latest",
         "sourceType": "module",
+        ecmaFeatures: {
+            jsx: true,
+        },
     },
     "plugins": [
         "react",
