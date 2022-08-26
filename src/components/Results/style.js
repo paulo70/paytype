34ecca1e58b81b6410b4ca08abbaee59f94d90eb
Paulo Costa <paulofrontend@gmail.com>
2022-08-26 13:46:13 -0300

    adjust css results box

--- src/components/Results/style.js
+++ src/components/Results/style.js
@@ -8,11 +8,13 @@ export const BoxResults = styled.div`
   text-align: left;
   color: #0054FF;
   font-size: 20px;
+  border-bottom: 1px solid #0054FF;
 
   &{
     h3{
-      margin-top: 20px;
+      margin-top: 90px;
       margin-left: 20px;
+      margin-bottom: 10px;
     }
   }
 `
